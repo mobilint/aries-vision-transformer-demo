dcd220cc9b6a0d72f6c160510b75ffc512e42e3c
beomsu <beomsu@mobilint.com>
2026-03-06 18:09:37 +0900

    Audit npm

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  devIndicators: {
-    buildActivity: false,
-  },
+  devIndicators: false,
 };
 
 export default nextConfig;
